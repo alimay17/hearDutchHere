7ddda0c982cb68014ccdfdff36aa9dcac6711c9a
Aly Smith <alimay.smith@outlook.com>
2022-06-14 16:38:10 -0500

    adding content to changelog

---
+++ js/linksLists/changeLog.js
@@ -0,0 +1,45 @@
+export const changeLogCurrent = [
+  {
+    "name": "H in Dutch",
+    "url": "/_H.html",
+    "text": ""
+  },
+  {
+    "name": "W in Dutch",
+    "url": "/_W.html",
+    "text": ""
+  },
+  {
+    "name": "EU in Dutch",
+    "url": "/_EU.html",
+    "text": ""
+  },
+
+]
+
+export const changeLog2022 = [
+  {
+    "name": "H in Dutch",
+    "url": "/_H.html",
+    "text": ""
+  },
+  {
+    "name": "W in Dutch",
+    "url": "/_W.html",
+    "text": ""
+  },
+  {
+    "name": "EU in Dutch",
+    "url": "/_EU.html",
+    "text": ""
+  },
+
+]
+
+export const changeLog2021 = [
+  {
+    "name": "Home",
+    "url": "/index.html",
+    "text": ""
+  },
+]
